701d20a82c7d56998835c3065ed94302c030c00a
Gabriel J. Almeida <gjalmeida0309@gmail.com>
2024-11-02 14:16:40 -0300

    Update script.js

--- scr/script.js
+++ scr/script.js
@@ -1,1 +1,13 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const navbar = document.querySelector('.navbar');
 
+    function handleScroll() {
+        if (window.scrollY > 50) { 
+            navbar.classList.add('scrolled');
+        } else {
+            navbar.classList.remove('scrolled');
+        }
+    }
+
+    window.addEventListener('scroll', handleScroll);
+});
